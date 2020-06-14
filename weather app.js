ece1a522be414b5a5bb264e4fe7ebc0498a19f38
Jay-C-oding <65338920+Jay-C-oding@users.noreply.github.com>
2020-06-14 20:42:36 +0200

    weather done

--- weather app.js
+++ weather app.js
@@ -1,77 +1,25 @@
 
-/*
+const myTemp = document.querySelector('#temperature');
+const myHum = document.querySelector('#humidity')
 
-button.eventListner()
 
-async function getData() {
+addEventListener(onload, myWeather());
+
+
+async function myWeather() {
+
     const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric');
     const data = await response.json();
-    const temp = data.main["temp"];
-    const hum = data.main["humidity"];
+    const temp = data.main["temp"]
+    const hum = data.main["humidity"]
 
+    myTemp.innerText = temp
+    myHum.innerText = hum
 
-    document.querySelector('#temperature').innerHTML = temp;
-    document.querySelector('#humidity').innerHTML = hum;
-}
+    let minutes = sec*60
+    let second = 1000
 
-
-fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-.then(response => response.json())
-    .then(data => console.log(data))
-.catch(err => alert("oops!"))
-const tempElement = document.querySelector('#temperature');
-const humElement = document.querySelector('#humidity');
-const weather = {}
- */
-
-
-function getWeather(){
-     fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-        .then(response = response.json();)
-        .then(data => {
-            console.log(data);
-
-        })
-         .then(function () {
-         });
-}
-getWeather()
-
-
-
-
-/*
-function myData(){
-    const myobj = fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-.then(response => response.json())
-        .then(data => console.log(data["main"]))
-
-        .catch(err => alert("wrong!"))
-
-    console.log(myobj);
-}
-
-function getData() {
-    const myApi =
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-            .then(response => response.json())
-            .then(data => console.log(data["main"]))
-            .catch(err => alert("wrong!"))
-
-
-    console.log(myApi)
-}
+    setInterval(myWeather, 15*minutes);
 
 }
-t = fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-    .then(response => response.json())
-    .then(data => console.log(data.main["temp"]))
-    .catch(err => alert("wrong!"))
- = t
-
-h = fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric')
-    .then(response => response.json())
-    .then(data => console.log(data.main["humidity"]))
-    .catch(err => alert("wrong!"))
-document.getElementById('humidity').innerHTML = h
-*/
+myWeather()
