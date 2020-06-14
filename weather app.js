5dec94bca842290aecd7604776928ded98f50aa5
Jay-C-oding <65338920+Jay-C-oding@users.noreply.github.com>
2020-06-15 01:32:02 +0200

    Update weather app.js

--- weather app.js
+++ weather app.js
@@ -8,7 +8,7 @@ addEventListener(onload, myWeather());
 
 async function myWeather() {
 
-    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric');
+    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Hannover&appid=2dcbd43d2ad7f40cdea842c3161a179d&units=metric');
     const data = await response.json();
     const temp = data.main["temp"]
     const hum = data.main["humidity"]
